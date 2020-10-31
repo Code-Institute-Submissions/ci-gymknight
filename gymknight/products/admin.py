fad91619b98a82b3f4e02ceeb4e7e269571e515f
ib-skoric <ib.skoric@outlook.com>
2020-10-31 14:54:31 +0000

    Register admin views

--- gymknight/products/admin.py
+++ gymknight/products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'price',
+        'rating',
+        'image',
+        'category'
+    )
+    
+    ordering = ('sku', )
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
